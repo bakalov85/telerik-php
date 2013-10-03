eccead9bcce2e26758013e3533cd818d14a7a48b
Ivan Ivanov <ivan.ivanov@suricactus.com>
2013-10-03 22:06:48 +0300

    Remove dir

--- slavi/homework1/includes/header.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <link rel="stylesheet" href="includes\styles.css"/>
-        <title><?=$pageTitle;?></title>
-    </head>
-    <body>
